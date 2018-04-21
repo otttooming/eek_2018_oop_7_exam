50c8c789fc79e2d35c0f7697b6d2ddf1bbfdd06c
Ott Tooming <OttTooming@gmail.com>
2018-04-21 14:13:27 +0300

    Initial commit

---
+++ eek_exam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eek_exam
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
