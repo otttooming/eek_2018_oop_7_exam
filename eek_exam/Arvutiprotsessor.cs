4fba28a2a9d32ec0412f24ab0cb9d9f8f0b510b0
Ott Tooming <OttTooming@gmail.com>
2018-04-21 15:29:45 +0300

    Add Arvutiprotsessor and Minuarvutiprotsessor classes

---
+++ eek_exam/Arvutiprotsessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace eek_exam
+{
+    class Arvutiprotsessor : Protsessor
+    {
+        public string firma;
+
+        public Arvutiprotsessor(double sagedus = 0, string firma = "---")
+            :base(sagedus)
+        {
+            this.firma = firma;
+        }
+
+        public Arvutiprotsessor(Arvutiprotsessor ob) : base(ob) { this.firma = ob.firma; }
+    }
+}
---
+++ eek_exam/Minuarvutiprotsessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace eek_exam
+{
+    class Minuarvutiprotsessor : Arvutiprotsessor
+    {
+        public double hind;
+
+        public Minuarvutiprotsessor(double sagedus = 0, string firma = "---", double hind = 0)
+            : base(sagedus, firma)
+        {
+            this.hind = hind;
+        }
+
+        public Minuarvutiprotsessor(Minuarvutiprotsessor ob) : base(ob) { this.hind = ob.hind; }
+    }
+}
